0d6f835ee2178d2e6b717cdb4a7925d96fe7f385
fareedkamal <fkamalars@gmail.com>
2024-07-02 19:35:35 +0500

    fixed src urls

--- components/dashboard/tickets/page.tsx
+++ components/dashboard/tickets/page.tsx
@@ -16,17 +16,17 @@ const Ticket: React.FC<TicketProps> = (props) => {
     <div className={`${props.className} lg:px-32 px-10`}>
       <div className='grid lg:grid-cols-3 lg:gap-24 gap-5 w-full'>
         <MarkTicket
-          mark='https://damnedventures.com/wp-content/uploads/4payments6weeks0interest-white-1.png'
+          mark='https://admin.damneddesigns.com/wp-content/uploads/4payments6weeks0interest-white-1.png'
           text='Buy now, Pay Later. 0% interest'
           onShow={setSezzle}
         />
         <MarkTicket
-          mark='https://damnedventures.com/wp-content/uploads/1_jLAS3yLE6G-ERUcNhAdWUA-Copy-1.png'
+          mark='https://admin.damneddesigns.com/wp-content/uploads/1_jLAS3yLE6G-ERUcNhAdWUA-Copy-1.png'
           text='Free shipping on US orders over $100'
           onShow={setShipping}
         />
         <MarkTicket
-          mark='https://damnedventures.com/wp-content/uploads/Asset-4-1.png'
+          mark='https://admin.damneddesigns.com/wp-content/uploads/Asset-4-1.png'
           text='Military and first responder discounts'
           onShow={setSheerMark}
         />
