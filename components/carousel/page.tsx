8dd9bc014eba6e99978799b1b9c37476dc33db34
fareedkamal <fkamalars@gmail.com>
2024-07-19 01:07:10 +0500

    fixes

--- components/carousel/page.tsx
+++ components/carousel/page.tsx
@@ -7,17 +7,39 @@ interface CarouselProps {
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
-import { Loader } from '../utils';
+import { useState } from 'react';
+import { CircularProgress } from '@mui/material';
 
 const ImageCarousel = ({ images }: CarouselProps) => {
   return (
     <Carousel>
       {images.map((image, index) => (
-        // <Image key={index} src={image} alt='' height={1000} width={1000} />
-        <img key={index} src={image} alt='' />
+        <CustomImage key={index} image={image} />
+        //<Image key={index} src={image} alt='' width={500} height={500} />
+        //<img key={index} src={image} alt='' />
       ))}
     </Carousel>
   );
 };
 
+const CustomImage = ({ image }: any) => {
+  const [loading, setLoading] = useState(true);
+  return (
+    <div className='h-full w-full'>
+      {loading ? (
+        <div className='bg-stone-200 h-full flex w-full'>
+          <CircularProgress sx={{ color: 'grey', m: 'auto' }} />
+        </div>
+      ) : null}
+
+      <Image
+        src={image}
+        alt=''
+        width={500}
+        height={500}
+        onLoadingComplete={() => setLoading(false)}
+      />
+    </div>
+  );
+};
 export default ImageCarousel;
